58553b69e5a085c59ed8beb1e2ab9d6a2cf693c1
Manami Kokado <kokado313.mnm@gmail.com>
2026-06-03 20:21:20 +0900

    NextConfig修正

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/my-portfolio' : '',
   images: {
     unoptimized: true, // Static Export用
